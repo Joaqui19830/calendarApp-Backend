e1eea805527cef5c36ad3d87a4d96372fdf6f366
Joaquin <joaqui2608.jc12@gmail.com>
2024-01-29 17:41:52 -0300

    Path * agregado

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
 
-
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
 
 
 
